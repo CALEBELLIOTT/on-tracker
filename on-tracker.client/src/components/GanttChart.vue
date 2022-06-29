0ef46c7beb72c0c0d85d62f8f866498ffd1a357a
Dane Barber <dane.a.barber@gmail.com>
2022-06-29 11:27:17 -0600

    Mongoose and ProjectSchema with everything else :P

---
+++ on-tracker.client/src/components/GanttChart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="component">
+
+
+  </div>
+</template>
+
+
+<script>
+import { GChart } from "vue3-googl-chart";
+
+export default {
+  components: {
+    GChart,
+  },
+  setup(){
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
