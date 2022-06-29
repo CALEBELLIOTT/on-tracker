8a160f53a658aec4e4b35f79afc14a91a57d8475
Seth McCormick <seth7293@gmail.com>
2022-06-29 15:16:30 -0600

    notes crud methods

--- on-tracker/server/controllers/NotesController.js
+++ on-tracker/server/controllers/NotesController.js
@@ -1,9 +1,41 @@
+import { notesService } from "../services/NotesService";
 import BaseController from "../utils/BaseController";
 
 export class NotesController extends BaseController {
     constructor() {
         super('api/notes')
         this.router
+            .get('/:id', this.getById)
+            .post('', this.createNote)
+            .put('/:id', this.editNote)
+        // .delete('/:id', this.edit)
+    }
 
+
+    async getById(req, res, next) {
+        try {
+            const notes = await notesService.getById(req.params.id)
+            res.send(notes)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async createNote(req, res, next) {
+        try {
+            const note = await notesService.createNote(req.body)
+            res.send(note)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async editNote(req, res, next) {
+        try {
+            const note = await notesService.editNote(req.body, req.params.id)
+            res.send(note)
+        } catch (error) {
+            next(error)
+        }
     }
 }
--- on-tracker/server/models/Note.js
+++ on-tracker/server/models/Note.js
@@ -4,5 +4,7 @@ const ObjectId = Schema.Types.ObjectId
 
 
 export const NoteSchema = new Schema({
-
+    projectId: { type: ObjectId, required: true, ref: 'Project' },
+    accountId: { type: ObjectId, required: true, ref: 'Account' },
+    body: { type: String, required: true }
 })
