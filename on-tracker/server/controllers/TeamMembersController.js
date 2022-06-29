39e11e5d1af410e20785a0acd0c6f766942f6f54
Diego <diegodominguezf40@gmail.com>
2022-06-29 16:37:58 -0600

    updated get teammebers

---
+++ on-tracker/server/controllers/TeamMembersController.js
@@ -0,0 +1,33 @@
+import BaseController from '../utils/BaseController'
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import { teamMembersService } from '../services/TeamMembersService'
+
+
+
+export class TeamMembersController extends BaseController {
+    constructor() {
+        super('api/teammembers')
+        this.router
+            .get('', this.getTeamMembers)
+            .use(Auth0Provider.getAuthorizedUserInfo)
+            .delete('/:id', this.removeTeamMembers)
+    }
+    async getTeamMembers(req, res, next) {
+        try {
+            const teamMembers = await teamMembersService.getTeamMembers()
+            return res.send(teamMembers)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async removeTeamMembers(req, res, next) {
+        try {
+
+            const teamMembers = await teamMembersService.removeTeamMembers(req.params.id, req.userInfo.id)
+            return res.send('delorted')
+        } catch (error) {
+            next(error)
+        }
+    }
+}
