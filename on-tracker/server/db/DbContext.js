6a03e1dc44b3b201ec87eb41d145e475fde4664e
Caleb Elliott <caleb123elliott@gmail.com>
2022-06-29 11:06:03 -0600

    edit dbContext

--- on-tracker/server/db/DbContext.js
+++ on-tracker/server/db/DbContext.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
+import { TaskSchema } from "../models/Task";
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+
+  Task = mongoose.model('Task', TaskSchema)
 }
 
 export const dbContext = new DbContext()
