2df4a5be962fc665f36584bff964e76b73e28b4f
Caleb Elliott <caleb123elliott@gmail.com>
2022-06-29 12:06:37 -0600

    tasks done

--- on-tracker/server/controllers/TasksController.js
+++ on-tracker/server/controllers/TasksController.js
@@ -9,6 +9,9 @@ export class TasksController extends BaseController {
     this.router
       .get('/:id', this.getById)
       .post('', this.createTask)
+      .put('/:id', this.editTask)
+      .put('/:id/completed', this.toggleCompleted)
+      .delete('/:id', this.removeTask)
   }
 
 
@@ -25,6 +28,33 @@ export class TasksController extends BaseController {
     try {
       const task = await tasksService.createTask(req.body)
       return res.send(task)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editTask(req, res, next) {
+    try {
+      const task = await tasksService.editTask(req.body, req.params.id)
+      return res.send(task)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async toggleCompleted(req, res, next) {
+    try {
+      const completed = await tasksService.toggleCompleted(req.params.id)
+      return res.send(completed)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async removeTask(req, res, next) {
+    try {
+      const task = await tasksService.removeTask(req.params.id)
+      return res.send('delorted')
     } catch (error) {
       next(error)
     }
--- on-tracker/server/services/TasksService.js
+++ on-tracker/server/services/TasksService.js
@@ -2,6 +2,7 @@ import { dbContext } from "../db/DbContext"
 
 
 class TasksService {
+
   async getById(id) {
     const task = await dbContext.Tasks.findById(id)
     return task
@@ -9,6 +10,27 @@ class TasksService {
   async createTask(taskData) {
     const task = await dbContext.Tasks.create(taskData)
     return task
+  }
+
+  async editTask(update, id) {
+    const original = await this.getById(id)
+    original.description = update.description || original.description
+    original.estimatedTime = update.estimatedTime || original.estimatedTime
+    await original.save()
+    return original
+  }
+
+  async toggleCompleted(id) {
+    const original = await this.getById(id)
+    original.isCompleted ? original.isCompleted = false : original.isCompleted = true
+    await original.save()
+    return original
+  }
+
+  async removeTask(id) {
+    const task = await this.getById(id)
+    await task.delete()
+    return
   }
 
 }
