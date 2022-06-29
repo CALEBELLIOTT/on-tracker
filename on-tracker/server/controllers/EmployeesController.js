89054f0f768f2ca3c2ae6e5ea8620dc225c312c0
Diego <diegodominguezf40@gmail.com>
2022-06-29 15:12:29 -0600

    added employee updated

--- on-tracker/server/controllers/EmployeesController.js
+++ on-tracker/server/controllers/EmployeesController.js
@@ -11,7 +11,9 @@ export class EmployeesController extends BaseController{
         super('api/employees')
         this.router
         .get('/:id/teammembers', this.teamByEmployee)
+        .use(Auth0Provider.getAuthorizedUserInfo)
         .post('', this.createEmployee)
+        .put('/:id', this.editEmployee)
     }
     
     async teamByEmployee(req, res, next) {
@@ -25,8 +27,18 @@ export class EmployeesController extends BaseController{
 
     async createEmployee(req, res, next) {
         try {
+            req.body.accountId = req.userInfo.id
             const employee = await employeesService.createEmployee(req.body)
             return res.send(employee)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async editEmployee(req, res, next){
+        try {
+           
+            const employee = await employeesService.editEmployee(req.body,  req.userInfo.id, req.params.id)
         } catch (error) {
             next(error)
         }
--- on-tracker/server/services/EmployeesService.js
+++ on-tracker/server/services/EmployeesService.js
@@ -1,12 +1,27 @@
 import { dbContext } from "../db/DbContext"
+import { Forbidden } from "../utils/Errors"
 
 
 
 class EmployeesService{
+    
     async createEmployee(employeeData) {
        const employee = await dbContext.Employees.create(employeeData)
        await employee.Populate('account', 'name picture')
        return employee
+    }
+
+    async editEmployee(update, userId, employeeId) {
+       const original = await dbContext.Employees.findById(employeeId)
+       if (original.accountId != userId) {
+        throw new Forbidden('This is not your account')
+       }
+       original.skills = update.skills || original.skills
+       original.certifications = update.certifications || original.certifications
+
+       await original.save()
+       await original.populate('account')
+       return original
     }
   
     
