5f30aefc76c229e4c58917a905756f34f692de2d
Caleb Elliott <caleb123elliott@gmail.com>
2022-06-29 10:57:14 -0600

    envs

--- on-tracker.client/src/env.js
+++ on-tracker.client/src/env.js
@@ -1,6 +1,8 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-k0ett2hk.us.auth0.com'
+export const clientId = 'duZCEjAYwATyvUJravqqcShyEj95dzss'
+export const audience = 'https://DiegoDev.com'
+
+
--- on-tracker/client/app/env.js
+++ on-tracker/client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-k0ett2hk.us.auth0.com'
+export const audience = 'https://DiegoDev.com'
+export const clientId = 'duZCEjAYwATyvUJravqqcShyEj95dzss'
