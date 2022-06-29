2d00c3fc0cef2e911e9d17ed6425e678bf1bdb7e
Seth McCormick <seth7293@gmail.com>
2022-06-29 15:16:12 -0600

    Notes crud methods

--- on-tracker/server/services/NotesService.js
+++ on-tracker/server/services/NotesService.js
@@ -1,4 +1,22 @@
+import { dbContext } from "../db/DbContext"
+
 class NotesService {
+
+    async getById(id) {
+        const notes = await dbContext.Notes.findById(id)
+        return notes
+    }
+
+    async createNote(noteData) {
+        const note = dbContext.Notes.create(noteData)
+        return note
+    }
+
+    async editNote(update, id) {
+        const original = await this.getById(id)
+        original.
+
+    }
 
 
 }
