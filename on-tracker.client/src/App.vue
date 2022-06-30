ee130c9a429de0e2e07ba1e684d63757fae44986
Timothy McCormick <breaking490@gmail.com>
2022-06-30 09:40:24 -0600

    background adjustment

--- on-tracker.client/src/App.vue
+++ on-tracker.client/src/App.vue
@@ -1,15 +1,7 @@
 <template>
-  <header>
-    <Navbar />
-  </header>
   <main>
     <router-view />
   </main>
-  <footer>
-    <div class="bg-dark text-light text-center p-4">
-      Made with 💖 by CodeWorks
-    </div>
-  </footer>
 </template>
 
 <script>
@@ -26,5 +18,4 @@ export default {
 </script>
 <style lang="scss">
 @import "./assets/scss/main.scss";
-
 </style>
--- on-tracker.client/src/pages/FirstTimeLoginPage.vue
+++ on-tracker.client/src/pages/FirstTimeLoginPage.vue
@@ -6,11 +6,19 @@
         <h4>Choose your account type</h4>
       </div>
     </div>
-    <div class="row">
-      <div class="col-4 elevation-2 rounded bg-white"></div>
+    <div class="row justify-content-center align-items-center">
+      <div class="col-4">
+        <div class="elevation-2 rounded bg-white selection-card">
+          <h4>OnTracker for businesses</h4>
+        </div>
+      </div>
     </div>
-    <div class="row">
-      <div class="col-4 elevation-2 rounded bg-white"></div>
+    <div class="row justify-content-center align-items-center">
+      <div class="col-4">
+        <div class="elevation-2 rounded bg-white selection-card">
+          <h4>OnTracker for employees</h4>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,5 +36,12 @@ export default {
 <style lang="scss" scoped>
 .dark-theme {
   background-color: rgba(37, 36, 36, 1);
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  flex-grow: 1;
+}
+.selection-card {
+  height: 20vh;
 }
 </style>
