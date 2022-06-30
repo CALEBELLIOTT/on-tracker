4298e9318eda2a4726624ad94b6364ed0af6ca01
Timothy McCormick <breaking490@gmail.com>
2022-06-30 09:31:25 -0600

    commit

--- on-tracker.client/src/pages/FirstTimeLoginPage.vue
+++ on-tracker.client/src/pages/FirstTimeLoginPage.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="container-fluid dark-theme"></div>
+  <div class="container-fluid dark-theme">
+    <div class="row">
+      <div class="col-12 text-center text-light">
+        <h4>We've noticed you're new here</h4>
+        <h4>Choose your account type</h4>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-4 elevation-2 rounded bg-white"></div>
+    </div>
+    <div class="row">
+      <div class="col-4 elevation-2 rounded bg-white"></div>
+    </div>
+  </div>
 </template>
 
 
