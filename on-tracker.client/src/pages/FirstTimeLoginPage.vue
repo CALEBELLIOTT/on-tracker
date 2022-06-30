aba14a92c2d49d3328317d8fdb464cda2a0748bb
Timothy McCormick <breaking490@gmail.com>
2022-06-30 09:52:51 -0600

    login page styling

--- on-tracker.client/src/pages/FirstTimeLoginPage.vue
+++ on-tracker.client/src/pages/FirstTimeLoginPage.vue
@@ -1,21 +1,41 @@
 <template>
-  <div class="container-fluid dark-theme">
-    <div class="row">
+  <div class="container-fluid d-flex justify-content-around dark-theme">
+    <div class="row mt-5">
       <div class="col-12 text-center text-light">
         <h4>We've noticed you're new here</h4>
         <h4>Choose your account type</h4>
       </div>
     </div>
     <div class="row justify-content-center align-items-center">
-      <div class="col-4">
-        <div class="elevation-2 rounded bg-white selection-card">
+      <div class="col-5">
+        <div
+          class="
+            d-flex
+            justify-content-center
+            align-items-center
+            elevation-2
+            rounded
+            bg-white
+            selection-card
+          "
+        >
           <h4>OnTracker for businesses</h4>
         </div>
       </div>
     </div>
-    <div class="row justify-content-center align-items-center">
-      <div class="col-4">
-        <div class="elevation-2 rounded bg-white selection-card">
+    <div class="row mb-5 justify-content-center align-items-center">
+      <div class="col-5">
+        <div
+          class="
+            d-flex
+            justify-content-center
+            align-items-center
+            elevation-2
+            rounded
+            bg-white
+            selection-card
+          "
+        >
           <h4>OnTracker for employees</h4>
         </div>
       </div>
@@ -42,6 +62,14 @@ export default {
   flex-grow: 1;
 }
 .selection-card {
-  height: 20vh;
+  height: 30vh;
+  transition: 500ms;
+  border: solid 4px rgba(37, 36, 36, 1);
+}
+.selection-card:hover {
+  cursor: pointer;
+  transform: scale(1.1);
+  transition: 500ms;
+  border: solid 4px rgb(255, 132, 73);
 }
 </style>
