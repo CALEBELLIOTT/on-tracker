e150b6142255fcf922118f512168074ee3a258f8
Timothy McCormick <breaking490@gmail.com>
2022-06-30 12:07:55 -0600

    added home page login

---
+++ on-tracker.client/src/components/UserLogin.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-5">
+        <div>
+          <img class="img-fluid" src="https://thiscatdoesnotexist.com" alt="" />
+          <h5>
+            On Tracker is a project management system designed to increase
+            productivity
+          </h5>
+          <p class="text-muted">
+            On Tracker is utilized by hundreds of companies around the world!
+          </p>
+          <h5 class="text-end">
+            On Tracker is a project management system designed to increase
+            productivity
+          </h5>
+          <p class="text-end text-muted">
+            On Tracker is utilized by hundreds of companies around the world!
+          </p>
+          <h5>
+            On Tracker is a project management system designed to increase
+            productivity
+          </h5>
+          <p class="text-muted">
+            On Tracker is utilized by hundreds of companies around the world!
+          </p>
+        </div>
+      </div>
+      <div class="col-7"></div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
