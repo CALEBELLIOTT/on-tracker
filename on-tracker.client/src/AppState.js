3de896adede787fb87cd412a4d764b775564f41d
Dane Barber <dane.a.barber@gmail.com>
2022-07-01 09:22:17 -0600

    employee stuff start

--- on-tracker.client/src/AppState.js
+++ on-tracker.client/src/AppState.js
@@ -7,5 +7,6 @@ export const AppState = reactive({
   projects: [],
   allBusinesses: [],
   activeProject: {},
-  projectNotes: []
+  projectNotes: [],
+  activeEmployee: {}
 })
---
+++ on-tracker.client/src/pages/EmployeesPage.vue
@@ -0,0 +1,21 @@
+<template>
+<!-- component of active employee -->
+  <div class="">
+
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup(){
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
