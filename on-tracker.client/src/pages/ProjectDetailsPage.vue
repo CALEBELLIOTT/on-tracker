a31ffaef398a26abb63a66afc1c60a84ee5a5ee2
Timothy McCormick <breaking490@gmail.com>
2022-07-01 12:35:51 -0600

    notes added

--- on-tracker.client/src/pages/ProjectDetailsPage.vue
+++ on-tracker.client/src/pages/ProjectDetailsPage.vue
@@ -6,8 +6,44 @@
     </div>
 
     <!-- Note Component -->
-    <div class="row">
-      <ProjectNotes v-for="n in notes" :key="n.id" :note="n" />
+    <div class="row align-items-center flex-column">
+      <div
+        class="
+          col-md-10
+          bg-grey
+          elevation-2
+          rounded
+          d-flex
+          justify-content-center
+        "
+      >
+        <div class="col-md-10">
+          <h1
+            class="text-center text-white border-bottom border-5 border-white"
+          >
+            <b>Notes</b>
+          </h1>
+          <ProjectNotes v-for="n in notes" :key="n.id" :note="n" />
+          <div class="d-flex justify-content-center">
+            <form @submit.prevent="createNote">
+              <textarea
+                placeholder="Add a note..."
+                name=""
+                id=""
+                cols="80"
+                rows="1"
+                v-model="noteData.body"
+              ></textarea>
+              <button
+                class="btn btn-dark text-light rounded mb-3"
+                type="submit"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
     <!--  -->
   </div>
@@ -15,7 +51,7 @@
 
 
 <script>
-import { computed, onMounted } from '@vue/runtime-core'
+import { computed, onMounted, ref } from '@vue/runtime-core'
 import { useRoute } from 'vue-router'
 import { logger } from '../utils/Logger'
 import Pop from '../utils/Pop'
@@ -25,6 +61,7 @@ import { notesService } from '../services/NotesService'
 export default {
 
   setup() {
+    let noteData = ref({})
     const route = useRoute()
     onMounted(async () => {
       try {
@@ -38,8 +75,20 @@ export default {
     )
 
     return {
+      noteData,
       project: computed(() => AppState.activeProject),
       notes: computed(() => AppState.projectNotes),
+      async createNote() {
+        try {
+          noteData.value.projectId = route.params.id
+          await notesService.createNote(noteData.value)
+        } catch (error) {
+          logger.error(error)
+          Pop.toast(error.message, 'error')
+        }
+
+      }
+
     }
   }
 }
