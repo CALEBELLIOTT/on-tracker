05061396baa3f7954750cec1eea2214a7a5f1b59
Diego <diegodominguezf40@gmail.com>
2022-07-12 16:21:41 -0600

    pushing

--- on-tracker.client/src/pages/ProjectDetailsPage.vue
+++ on-tracker.client/src/pages/ProjectDetailsPage.vue
@@ -3,14 +3,24 @@
 
   <div class="container-fluid dark-theme">
     <div class="mt-3 me-0">
-      <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
-        aria-controls="offcanvasRight">
+      <button
+        class="btn btn-primary"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasRight"
+        aria-controls="offcanvasRight"
+      >
         Tasks
       </button>
     </div>
     <div>
-      <button class="btn btn-primary mt-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#availableEmployees"
-        aria-controls="offcanvasRight">
+      <button
+        class="btn btn-primary mt-2"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#availableEmployees"
+        aria-controls="offcanvasRight"
+      >
         Available Employees
       </button>
     </div>
@@ -24,16 +34,20 @@
 
     <!-- Note Component -->
     <div class="row align-items-center flex-column">
-      <div class="
+      <div
+        class="
           col-md-10
           grey-card
           elevation-2
           rounded
           d-flex
           justify-content-center
-        ">
+        "
+      >
         <div class="col-md-10 m-2">
-          <h1 class="text-center text-white border-bottom border-5 border-white">
+          <h1
+            class="text-center text-white border-bottom border-5 border-white"
+          >
             <b><em>Notes</em></b>
           </h1>
           <div class="notes-section m-4 bg-light rounded">
@@ -41,10 +55,20 @@
           </div>
           <div class="d-flex justify-content-center">
             <form @submit.prevent="createNote">
-              <textarea class="form-control" placeholder="Add a note..." name="" id="" cols="80" rows="1"
-                v-model="noteData.body"></textarea>
+              <textarea
+                class="form-control"
+                placeholder="Add a note..."
+                name=""
+                id=""
+                cols="80"
+                rows="1"
+                v-model="noteData.body"
+              ></textarea>
               <div class="d-flex justify-content-center mt-2">
-                <button class="btn btn-dark text-light rounded mb-3" type="submit">
+                <button
+                  class="btn btn-dark text-light rounded mb-3"
+                  type="submit"
+                >
                   Submit
                 </button>
               </div>
@@ -52,121 +76,16 @@
           </div>
         </div>
       </div>
+      <div class="row pt-5 py-5">
+        <div>
+          <div class="col-12 bg-white elevation-4 rounded border border-4 p-5">
+            hello
+          </div>
+        </div>
+      </div>
     </div>
 
     <!-- Employee Cards and Mapbox  -->
-    <div class="row justify-content-around">
-      <div class="col-md-3 d-flex justify-content-center flex-column mt-4">
-        <h4 class="text-center text-light">Current</h4>
-        <h4 class="text-center text-light">Employees</h4>
-        <div class="
-            employee-cards
-            elevation-2
-            rounded
-            grey-card
-            m-4
-            d-flex
-            flex-column
-            justify-content-center
-          ">
-          <div class="col-12 d-flex justify-content-around p-2">
-            <span><img class="small-profile-img"
-                src="https://th.bing.com/th/id/R.3223efb84d9394750bcb90dadfefc5b2?rik=zhNwcP5qVVgiFQ&pid=ImgRaw&r=0"
-                alt="" /></span>
-            <span>
-              <h2 class="text-white">
-                <b><em>Bob</em></b>
-              </h2>
-            </span>
-            <span><i class="text-white mdi mdi-delete f-24"></i></span>
-          </div>
-          <div class="col-12 d-flex justify-content-around p-2">
-            <span><img class="small-profile-img"
-                src="https://th.bing.com/th/id/R.3223efb84d9394750bcb90dadfefc5b2?rik=zhNwcP5qVVgiFQ&pid=ImgRaw&r=0"
-                alt="" /></span>
-            <span>
-              <h2 class="text-white">
-                <b><em>Bob</em></b>
-              </h2>
-            </span>
-            <span><i class="text-white mdi mdi-delete f-24"></i></span>
-          </div>
-          <div class="col-12 d-flex justify-content-around p-2">
-            <span><img class="small-profile-img"
-                src="https://th.bing.com/th/id/R.3223efb84d9394750bcb90dadfefc5b2?rik=zhNwcP5qVVgiFQ&pid=ImgRaw&r=0"
-                alt="" /></span>
-            <span>
-              <h2 class="text-white">
-                <b><em>Bob</em></b>
-              </h2>
-            </span>
-            <span><i class="text-white mdi mdi-delete f-24"></i></span>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3 d-flex justify-content-center flex-column mt-4">
-        <h4 class="text-center text-light">Available</h4>
-        <h4 class="text-center text-light">Employees</h4>
-        <div class="
-            employee-cards
-            d-flex
-            flex-column
-            elevation-2
-            rounded
-            grey-card
-            m-4
-            justify-content-center
-          ">
-          <div class="col-12 d-flex justify-content-around p-2">
-            <span><img class="small-profile-img"
-                src="https://th.bing.com/th/id/R.3223efb84d9394750bcb90dadfefc5b2?rik=zhNwcP5qVVgiFQ&pid=ImgRaw&r=0"
-                alt="" /></span>
-            <span>
-              <h2 class="text-white">
-                <b><em>Bob</em></b>
-              </h2>
-            </span>
-            <span><i class="text-white mdi mdi-checkbox-marked f-22"></i></span>
-          </div>
-          <div class="col-12 d-flex justify-content-around p-2">
-            <span><img class="small-profile-img"
-                src="https://th.bing.com/th/id/R.3223efb84d9394750bcb90dadfefc5b2?rik=zhNwcP5qVVgiFQ&pid=ImgRaw&r=0"
-                alt="" /></span>
-            <span>
-              <h2 class="text-white">
-                <b><em>Bob</em></b>
-              </h2>
-            </span>
-            <span><i class="text-white mdi mdi-checkbox-marked f-22"></i></span>
-          </div>
-          <div class="col-12 d-flex justify-content-around p-2">
-            <span><img class="small-profile-img"
-                src="https://th.bing.com/th/id/R.3223efb84d9394750bcb90dadfefc5b2?rik=zhNwcP5qVVgiFQ&pid=ImgRaw&r=0"
-                alt="" /></span>
-            <span>
-              <h2 class="text-white">
-                <b><em>Bob</em></b>
-              </h2>
-            </span>
-            <span><i class="text-white mdi mdi-checkbox-marked f-22"></i></span>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-6 d-flex justify-content-center flex-column mt-4">
-        <div class="
-            map-card
-            elevation-2
-            rounded
-            bg-light
-            m-4
-            d-flex
-            align-items-center
-          ">
-          <img class="img-fluid p-2 map-card-img" src="https://www.s-lec.eu/wp-content/uploads/map.jpg" alt="" />
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -205,6 +124,9 @@ export default {
       employees: computed(() => AppState.employees),
       project: computed(() => AppState.activeProject),
       notes: computed(() => AppState.projectNotes),
+      teamMembers: computed(() => AppState.activeProjectAvailableEmployees),
+
+
       async createNote() {
         try {
           noteData.value.projectId = route.params.id
